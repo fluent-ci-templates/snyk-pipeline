de1627659b04e8e0db5ebc39ec1e9ee3f4f3e700
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-22 11:05:02 +0000

    ci: use dagger v0.8.8

--- ci.ts
+++ ci.ts
@@ -1,10 +1,3 @@
-import Client, { connect } from "https://sdk.fluentci.io/v0.1.9/mod.ts";
-import { test } from "https://pkg.fluentci.io/snyk_pipeline@v0.1.1/mod.ts";
+import { test } from "https://pkg.fluentci.io/snyk_pipeline@v0.2.0/mod.ts";
 
-function pipeline(src = ".") {
-  connect(async (client: Client) => {
-    await test(client, src);
-  });
-}
-
-pipeline();
+await test();
