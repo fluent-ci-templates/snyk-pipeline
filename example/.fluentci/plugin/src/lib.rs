281edcefd6bad8f57e885acf1a04b3eb05b778b0
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-06 10:36:39 +0000

    fix example

--- example/.fluentci/plugin/src/lib.rs
+++ example/.fluentci/plugin/src/lib.rs
@@ -36,6 +36,96 @@ pub fn test(args: String) -> FnResult<String> {
 }
 
 #[plugin_fn]
+pub fn iac(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("iac test")?
+        .pkgx()?
+        .with_exec(vec![
+            "pkgx",
+            "+nodejs.org",
+            "+bun.sh",
+            "bunx",
+            "snyk",
+            "iac",
+            &args,
+        ])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn code(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("iac test")?
+        .pkgx()?
+        .with_exec(vec![
+            "pkgx",
+            "+nodejs.org",
+            "+bun.sh",
+            "bunx",
+            "snyk",
+            "code",
+            &args,
+        ])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn sbom(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("iac test")?
+        .pkgx()?
+        .with_exec(vec![
+            "pkgx",
+            "+nodejs.org",
+            "+bun.sh",
+            "bunx",
+            "snyk",
+            "sbom",
+            &args,
+        ])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn log4shell(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("iac test")?
+        .pkgx()?
+        .with_exec(vec![
+            "pkgx",
+            "+nodejs.org",
+            "+bun.sh",
+            "bunx",
+            "snyk",
+            "log4shell",
+            &args,
+        ])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn container(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("iac test")?
+        .pkgx()?
+        .with_exec(vec![
+            "pkgx",
+            "+nodejs.org",
+            "+bun.sh",
+            "bunx",
+            "snyk",
+            "container",
+            &args,
+        ])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
 pub fn iac_test(args: String) -> FnResult<String> {
     let stdout = dag()
         .pipeline("iac test")?
